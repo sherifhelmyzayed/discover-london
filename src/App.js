6976c45a534a734da3fb701735ebbc2533af9cb0
sherifhelmyzayed <sherifhelmyzayed@gmail.com>
2022-03-14 15:40:20 +0200

    minor styling edits

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Routes, Route, BrowserRouter } from "react-router-dom"
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Dashboard from './Pages/Dashboard';
 import Home from './Pages/Home';
+import Map from './Pages/Map'
 
 
 const mainTheme = createTheme({
   palette: {
-
     // essam choose of color set is presented here:
     primary: {
       main: '#0c2442',
@@ -19,40 +19,44 @@ const mainTheme = createTheme({
     purple: {
       main: '#17152e'
     },
+    newColor: {
+      main: "#fffff"
+    },
     white: '#ffffff'
   },
-  
+
   // Typography should be edit according to layout
   typography: {
     fontFamily: 'Montserrat',
     fontWeightLight: 100,
     fontWeightRegular: 200,
     fontWeightMedium: 200,
-    fontWeightBold: 100,
-    fontSize: '24',
+    fontWeightBold: 800,
+    fontSize: 24,
     lineHeight: '50',
     h1: {
-      fontSize: '30px'
+      fontSize: 30,
     },
     subtitle1: {
-      fontSize: '25px'
-    }
+      fontSize: 25
+    },
+    // body1 body subtitle1 subtitle2 caption button overline
   }
 
 })
-
 function App() {
   return (
-    <>
+
       <ThemeProvider theme={mainTheme}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/map" element={<Map />} />
             <Route path="dashboard" element={<Dashboard />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
-    </>
+
   )
 }
 
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,10 +1,40 @@
-import { Box, Container, Grid, Paper } from '@mui/material'
+import { useState } from 'react'
+import { Box, Grid, Paper } from '@mui/material'
 import Typography from '@mui/material/Typography'
-import '@fontsource/lato/300.css';
+import { makeStyles } from '@mui/styles';
 
-import React from 'react'
+import Header from '../Components/Dashboard/DashboardElements';
 
 const Dashboard = () => {
+
+    // new style by make style
+    const useStyles = makeStyles({
+        text: {
+            color: 'red',
+            fontSize: 40,
+            backgroundColor: 'white',
+            marginLeft: 50,
+            cursor: 'pointer',
+            '&:hover': {
+                backgroundColor: 'black',
+                color: 'white'
+            },
+        },
+        header: {
+        },
+        navbar: {
+        }
+    });
+
+
+
+    // use state hook
+    const [isOpen, setIsOpen] = useState();
+
+
+    // useStyle Hook
+    const classes = useStyles();
+
     return (
         <>
             <Grid
@@ -13,58 +43,70 @@ const Dashboard = () => {
                 justify="center"
                 alignItems="center"
             >
-                <Grid container md={3} bgcolor={"primary.main"} direction="column" paddingLeft={5} paddingTop={5}
+                <Grid container xs={12} md={3} bgcolor={"primary.main"} direction="column" paddingLeft={5} paddingTop={5}
                     justifyContent="start"
                     alignItems="start"
                     height="100vh"
-                    sx={{
-                        // custom css
-                    }}
+                    // above is the first styling method
                 >
-
-                    <Box>
-                        <item>button 1</item>
+                    {/* custome css using sx prop */}
+                    <Box sx={{
+                        margin: {
+                            md: '50px',
+                            sm: '20px'
+                        },
+                        color: {
+                            xs: "red",
+                            md: "yellow"
+                        },
+                        // custom css
+                    }}>
+                        <Typography>button 1</Typography>
                     </Box>
                     <Box>
-                        <item>button 2</item>
+                        {/* third methof for styling */}
+                        <Typography className={classes.text}>button 2</Typography>
                     </Box>
                     <Box>
-                        <item>button 3</item>
+                        <Typography>button 3</Typography>
                     </Box>
                     <Box>
-                        <item>button 4</item>
+                        <Typography>button 4</Typography>
                     </Box>
                     <Box>
-                        <item>button 5</item>
+                        <Typography>button 5</Typography>
                     </Box>
                     <Paper>
-                        <item>button 5</item>
+                        <Typography>button 5</Typography>
                     </Paper>
                     <Paper>
-                        <item>button 5</item>
+                        <Typography>button 5</Typography>
                     </Paper>
                 </Grid>
 
-                <Grid item md={9}>
-                    <Box>
-                        <Typography variant="h1" component="h1">
+                <Grid container md={9}>
+                    <div className="">
+                    </div>
+                    <Box >
+                        <Typography variant="h1" component="h5">
                             Helicopter
                         </Typography>
                     </Box>
-                    <Grid item xs={8}>
-                        <item>XS=8</item>
+                    <Grid item={true} xs={8}>
+                        <Typography>XS=8</Typography>
                     </Grid>
-                    <Grid item xs={4}>
-                        <item>XS=8</item>
+                    <Grid item={true} xs={4}>
+                        <Typography>XS=8</Typography>
                     </Grid>
-                    <Grid item xs={4}>
-                        <item>XS=8</item>
+                    <Grid item={true} xs={4}>
+                        <Typography>XS=8</Typography>
                     </Grid>
-                    <Grid item xs={8}>
-                        <item>XS=8</item>
+                    <Grid item={true} xs={8}>
+                        <Typography>XS=8</Typography>
                     </Grid>
                     <Box />
                     <Box />
+                    <Header propTest={true}> test header</Header>
                 </Grid>
             </Grid>
         </>
