09e028b9067649888c1c6376de2ec80338d30ad9
sherifhelmyzayed <sherifhelmyzayed@gmail.com>
2022-03-16 02:06:15 +0200

    modifed template font settings and uploaded left side of the hosting dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -3,6 +3,8 @@ import { Box, Grid, Paper } from '@mui/material'
 import Typography from '@mui/material/Typography'
 import { makeStyles } from '@mui/styles';
 
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 import Header from '../Components/Dashboard/DashboardElements';
 
 const Dashboard = () => {
@@ -43,70 +45,192 @@ const Dashboard = () => {
                 justify="center"
                 alignItems="center"
             >
-                <Grid container xs={12} md={3} bgcolor={"primary.main"} direction="column" paddingLeft={5} paddingTop={5}
+                <Grid item xs={12} md={3} bgcolor={"light"} paddingX={3} paddingTop={3}
                     justifyContent="start"
                     alignItems="start"
                     height="100vh"
-                    // above is the first styling method
+                // above is the first styling method
                 >
-                    {/* custome css using sx prop */}
-                    <Box sx={{
-                        margin: {
-                            md: '50px',
-                            sm: '20px'
-                        },
-                        color: {
-                            xs: "red",
-                            md: "yellow"
-                        },
-                        // custom css
-                    }}>
-                        <Typography>button 1</Typography>
-                    </Box>
+                    <Typography variant="h6" component="h5">
+                        Good afternoon,
+                    </Typography>
+                    <Typography variant="h5" component="h5">
+                        Sherif
+                    </Typography>
+
+                    <Grid container marginBottom={3} marginTop={3}>
+                        <Grid item xs={6}>
+                            <Typography variant="subtitle1" component="h5">
+                                Earnings in August
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} textAlign="end">
+                            <Typography variant="subtitle1" component="h5">
+                                $600
+                            </Typography>
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            <Typography variant="subtitle1" component="h5">
+                                Overall rating
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} textAlign="end">
+                            <Typography variant="subtitle1" component="h5">
+                                4.6
+                            </Typography>
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            <Typography variant="subtitle1" component="h5">
+                                30-day bookings
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} textAlign="end">
+                            <Typography variant="subtitle1" component="h5">
+                                6
+                            </Typography>
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            <Typography variant="subtitle1" component="h5">
+                                30-day views
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} textAlign="end">
+                            <Typography variant="subtitle1" component="h5">
+                                300
+                            </Typography>
+                        </Grid>
+                    </Grid>
+
+                    {/* Booking requests START */}
                     <Box>
-                        {/* third methof for styling */}
-                        <Typography className={classes.text}>button 2</Typography>
+                        <Typography variant="h5" component="h3" display="inline">
+                            Booking requests
+                        </Typography >
+                        <Box marginLeft={3} display="inline">
+                            <ArrowForwardIosIcon fontSize={'1px'} />
+                        </Box>
+                        <Typography variant="subtitle2" component="p">
+                            97% response rate
+                        </Typography>
+                        <Box marginTop={3}>
+                            <Paper
+                                sx={{
+                                    paddingTop: 1,
+                                    paddingBottom: 2,
+                                    bgcolor: 'transparent',
+                                    borderTop: 'none',
+                                    borderRight: 'none',
+                                    borderLeft: 'none'
+                                }}
+                                variant='outlined' paddingY={5}>
+                                <Typography variant="OVERLINE" component="p">
+                                    Inquiry from NAME
+                                </Typography>
+                                <Typography variant="subtitle2" component="p">
+                                    1 guest - Duration: 38 nights - Date: Sep23 to Oct 31 - Property Name
+                                </Typography>
+                                <Typography variant="clickable" component="p" color={"primary.light"}>
+                                    Respond
+                                </Typography>
+                            </Paper>
+                        </Box>
+                        <Box marginTop={3}>
+                            <Paper
+                                sx={{
+                                    paddingTop: 1,
+                                    paddingBottom: 2,
+                                    bgcolor: 'transparent',
+                                    borderTop: 'none',
+                                    borderRight: 'none',
+                                    borderLeft: 'none'
+                                }}
+                                variant='outlined' paddingY={5}>
+                                <Typography variant="OVERLINE" component="p">
+                                    Inquiry from NAME
+                                </Typography>
+                                <Typography variant="subtitle2" component="p">
+                                    1 guest - Duration: 38 nights - Date: Sep23 to Oct 31 - Property Name
+                                </Typography>
+                                <Typography variant="clickable" component="p" color={"primary.light"}>
+                                    Respond
+                                </Typography>
+                            </Paper>
+                        </Box>
+                        <Box marginTop={3}>
+                            <Paper
+                                sx={{
+                                    paddingTop: 1,
+                                    paddingBottom: 2,
+                                    bgcolor: 'transparent',
+                                    borderTop: 'none',
+                                    borderRight: 'none',
+                                    borderLeft: 'none'
+                                }}
+                                variant='outlined' paddingY={5}>
+                                <Typography variant="OVERLINE" component="p">
+                                    Inquiry from NAME
+                                </Typography>
+                                <Typography variant="subtitle2" component="p">
+                                    1 guest - Duration: 38 nights - Date: Sep23 to Oct 31 - Property Name
+                                </Typography>
+                                <Typography variant="clickable" component="p" color={"primary.light"}>
+                                    Respond
+                                </Typography>
+                            </Paper>
+                        </Box>
                     </Box>
-                    <Box>
-                        <Typography>button 3</Typography>
+                    {/* Booking requests END */}
+
+
+                    {/* To do Start */}
+                    <Box marginTop={5}>
+                        <Typography variant="h5" component="h3" display="inline">
+                            To-dos
+                        </Typography >
+                        <Box marginTop={3}>
+                            <Typography variant="subtitle2" component="p">
+                                You're all caught up!
+                            </Typography>
+                        </Box>
                     </Box>
-                    <Box>
-                        <Typography>button 4</Typography>
+                    {/* To do END */}
+
+                    {/* Notifications Start */}
+                    <Box marginTop={5}>
+                        <Typography variant="h5" component="h3" display="inline">
+                            Notifications
+                        </Typography >
+                        <Box marginTop={3}>
+                            <Paper
+                                sx={{
+                                    paddingTop: 1,
+                                    paddingBottom: 2,
+                                    bgcolor: 'transparent',
+                                    borderTop: 'none',
+                                    borderRight: 'none',
+                                    borderLeft: 'none'
+                                }}
+                                variant='outlined' paddingY={5}>
+
+                                <Typography variant="subtitle2" component="p">
+                                    Read the review that Sherif Helmy wrote by leaving one of your own. When yours is done, they'll be both be public
+                                </Typography>
+                                <Typography variant="subtitle2" component="p">
+                                    a day ago
+                                </Typography>
+                            </Paper>
+                        </Box>
                     </Box>
-                    <Box>
-                        <Typography>button 5</Typography>
-                    </Box>
-                    <Paper>
-                        <Typography>button 5</Typography>
-                    </Paper>
-                    <Paper>
-                        <Typography>button 5</Typography>
-                    </Paper>
+                    {/* Notifications END */}
+
+
                 </Grid>
 
-                <Grid container md={9}>
-                    <div className="">
-                    </div>
-                    <Box >
-                        <Typography variant="h1" component="h5">
-                            Helicopter
-                        </Typography>
-                    </Box>
-                    <Grid item={true} xs={8}>
-                        <Typography>XS=8</Typography>
-                    </Grid>
-                    <Grid item={true} xs={4}>
-                        <Typography>XS=8</Typography>
-                    </Grid>
-                    <Grid item={true} xs={4}>
-                        <Typography>XS=8</Typography>
-                    </Grid>
-                    <Grid item={true} xs={8}>
-                        <Typography>XS=8</Typography>
-                    </Grid>
-                    <Box />
-                    <Box />
-                    <Header propTest={true}> test header</Header>
+                <Grid item md={9}>
+                    graph is here
                 </Grid>
             </Grid>
         </>
