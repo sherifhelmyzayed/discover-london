a720a542308967b6e52829dc6de097a8af9e2d27
Ahmed nabil mohamed said ahmed mohamed zidan <anabil3020@gmail.com>
2022-03-20 17:32:31 +0200

    login and sign up form

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,14 @@
+import { Login } from '@mui/icons-material'
 import React from 'react'
 import Header from '../Components/Home/Header/Header'
 import Laptop from '../Components/Home/Laptop/Laptop'
 import Tablet from '../Components/Home/Tablet/Tablet'
+import Modal from '../Components/form/Modal'
 
 const Home = () => {
   return (
     <>
+       <Modal/>
         <Header />
         <Laptop />
         <Tablet />
